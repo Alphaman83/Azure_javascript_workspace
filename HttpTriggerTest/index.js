4c63194e01ac3379b3b213a151bb4fdd04e6fef9
Syed Amanullah <Syed.Amanullah@DISYS.IN>
2023-01-31 12:33:45 +0530

    Initial Commit

---
+++ HttpTriggerTest/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if(req.query.num1 && req.query.num2 || (req.body && req.body.name)){
+        const responseMessage = {
+            //status:200,Defaults to 200
+            body:req.query.num1 * req.query.num2
+        }
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
+    }
+    else{
+        context.res = {
+            status: 400, /* Defaults to 200 */
+            body: "Pass two numbers (num1 & num2) as query parameters"
+        }
+    }
+
+}
